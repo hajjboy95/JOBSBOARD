1e9ada88b59348297daa256e651e79eea2c75076
hajjboy95 <gizmo-boy95@hotmail.com>
2015-08-06 22:54:21 -0700

    working job_board , UI not the prettiest , without users

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,12 +4,16 @@ class JobsController < ApplicationController
   # GET /jobs
   # GET /jobs.json
   def index
-    puts "params #{params.inspect}"
+    puts "params #{params}"
     if params[:category].blank?
       @jobs = Job.order("created_at DESC")
     else
       @category_id = Category.find_by(name: params[:category]).id
       @jobs = Job.where(category_id: @category_id).order("created_at DESC")
+
+
+
+
 
     end
   end
